cacaf36b1ca6490d2ea754594395345cc3c18a37
Abhinav <abhinavchawda23@gmail.com>
2024-10-28 18:49:38 +0530

    Made some changes in content of the website

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -13,12 +13,12 @@ const AboutUs = () => {
                             <img src="/About/withDir.jpg" className='rounded-2xl md:h-[350px] md:w-[500px] mx-auto'></img>
                         </div>
 
-                        <div className='written-content w-3/4 lg:w-[40%] text-center'>
+                        <div data-aos="fade-left" offset="100" className='written-content w-3/4 lg:w-[40%] text-center'>
                             <div className='text-3xl font-blod my-5'>
-                                Evolve : The Electric Vehicle Club of MANIT Bhopal
+                                Evolve : The Electric Interdisciplinary Technical Club of MANIT Bhopal
                             </div>
                             <div className='text-lg'>
-                                Initiative for providing students with practical implementation of various industry-based technologies initiated under the Department of Electrical Engineering, NIT Bhopal.
+                                Evolve is an Initiative for providing students with practical implementation of various industry-based technologies initiated under the Department of Electrical Engineering, NIT Bhopal.
 
                                 Evolve is a dynamic technical club at Maulana Azad National Institute of Technology, Bhopal.
                                 Established in 2023  creating an environment that transforms ideas into impactful solutions through different events and collaborative initiatives, a shared enthusiasm for hands-on learning and inventive problem-solving. The club welcomes any student passionate about robotics, mechanical engineering, electrical engineering, and software development. Beyond the campus, the club has made its mark, bringing numerous prizes and enhancing prestige.
--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -9,20 +9,23 @@ const Achievements = () => {
 
                 <div className='flex flex-col lg:flex-row justify-center items-center gap-10 my-20'>
                     <div className='image w-3/4 lg:w-[40%] hover:scale-110 transition-transform duration-300 ease-in-out'>
-                        <img 
-                        data-aos="fade-right"
-                         src="Achievements/trophy.jpg" className='rounded-2xl md:h-[350px] md:w-[500px] mx-auto'></img>
+                        <img
+                            data-aos="fade-right"
+                            src="Achievements/trophy.jpg" className='rounded-2xl md:h-[350px] md:w-[500px] mx-auto'></img>
                     </div>
 
-                    <div className='written-content w-3/4 lg:w-[40%] text-center'>
-                        <div className='text-3xl font-blod my-5'>
-                            प्रभोत्सव Technical Quiz Competition 2024 “Quizzard”
+                    <div data-aos="fade-left" offset="100" className='written-content w-3/4 lg:w-[40%] text-center'>
+                        <div className='text-3xl font-blod mt-0 mb-2'>
+                            Gujrat Robofest 2024 : India's biggest Robotic competition
                         </div>
                         <div className='text-lg'>
-                            Thrilled to share the moment of triumph as our teams representing MANIT Bhopal during Quizzard at प्रभोTsav'24 and have clinched all the top three positions and, earned a well-deserved prize🏆
-                            It was a thrilling journey full of learning and adrenaline!
-                            Our dedication, teamwork, and passion for knowledge have truly shined through.
-                            Let's continue to challenge ourselves, learn from each other, and strive for excellence in everything we do. There are more milestones and success ahead!
+                            With effort and hard work, three teams from Evolve have advanced to the list of winners of the Ideation Stage. Competing with young minds from across India. Each team will now develop a solid proof of concept for their respective ideas under the categories they were selected for, with each team receiving a cash prize of INR 50,000 to move ahead.
+                        </div>
+                        <div className='text-3xl font-blod mt-8 mb-2'>
+                            National Finalist-TATA Technologies Innovent
+                        </div>
+                        <div className='text-lg'>
+                            Three Team Members (Mr. Yash Kumar Atlani, Mr. Ayush Jain, Mr. Aman Sharma) from Evolve were selected as the National Finalists for the Tata Technologies InnoVent. A National Hackathon for Engineering Students. The Team from Evolve NIT Bhopal was selected under the Top 10 in the Country out of 2900 Members and 800+ Teams.
                         </div>
                     </div>
 
