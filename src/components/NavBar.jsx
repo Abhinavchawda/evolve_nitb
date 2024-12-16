0dedcce1a48008be41392479059640d7bf7ce840
Gaurav Shukla <140684107+GodZilla-7@users.noreply.github.com>
2024-12-16 20:18:11 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,6 @@ import { useRef } from 'react';
 import bar_logo from '../assets/bar.svg';
 import cross_logo from '../assets/cross.svg';
 import home_logo from '../assets/home.svg';
-
 const NavBar = () => {
     const btn_ref = useRef(0);
     const icon_ref = useRef(0);
@@ -27,19 +26,19 @@ const NavBar = () => {
         let temp = document.getElementById("box");
         if (window.scrollY >= 90) {
             temp.classList.add("fixed");
-            temp.classList.add("scale-x-105");
+            temp.classList.add("scale-x-95");
         } else {
             temp.classList.remove("fixed");
-            temp.classList.remove("scale-x-105");    
+            temp.classList.remove("scale-x-95");    
         }
       }
 
     return (
         <div className='flex justify-center items-center'>
 
-        <div ref={bar_ref} id='box' className='w-[93%] mt-5 border border-[rgb(71,255,47)] hover:border-transparent rounded-3xl backdrop-blur-sm fixed top-1 z-50 mx-auto transition-all duration-500 ease-in-out'>
+        <div ref={bar_ref} id='box' className='w-[93%] mt-5 border border-transparent hover:border-[rgb(71,255,47)] rounded-3xl backdrop-blur-lg fixed top-1 z-50 mx-auto transition-all duration-500 ease-in-out'>
             <div className='flex items-center justify-between h-20  rounded-3xl'>
-                <div className='text-[rgb(17,42,42)] font-bold text-3xl mx-5 animate-pulse'>
+                <div className='text-[rgb(17,42,42)] font-bold text-3xl mx-5 animate-pulse ml-10'>
                     <img src='/Evolve_logo.png' height={50} width={75}></img>
                 </div>
 
@@ -47,23 +46,21 @@ const NavBar = () => {
                     <img ref={icon_ref} className='px-8 invert lg:hidden text-black' src={bar_logo} onClick={showMenu} alt='menu'></img>
                     <div>
                         <ul ref={btn_ref} className='invisible lg:hidden bg-[rgb(20,20,20,0.4)] text-white rounded-2xl p-5 px-8 absolute right-5 top-28 text-md'>
-                            <li className='rounded-xl font-semibold text-[rgb(71,255,47)] m-2 flex justify-center py-1 px-2'><a href='#main'><img className='invert' src={home_logo} alt='Home'></img></a></li>
+                            <li className='rounded-xl font-semibold text-border-[rgb(71,255,47)] m-2 flex justify-center py-1 px-2'><a href='#main'><img className='invert' src={home_logo} alt='Home'></img></a></li>
                             <li className='rounded-xl font-semibold text-[rgb(71,255,47)] m-2 text-center py-1 px-2'><a href='#about'>About</a></li>
                             <li className='rounded-xl font-semibold text-[rgb(71,255,47)] m-2 text-center py-1 px-2'><a href='#events'>Events</a></li>
-                            <li className='rounded-xl font-semibold text-[rgb(71,255,47)] m-2 text-center py-1 px-2'><a href='#projects'>Projects</a></li>
-                            <li className='rounded-xl font-semibold text-[rgb(71,255,47)] m-2 text-center py-1 px-2'><a href='#contact'>Contact</a></li>
-                            <li className='rounded-xl text-black bg-[rgb(71,255,47)] font-semibold px-3 py-1 text-center'><a href='http://google.com'>E-Cycle</a></li>
+
                         </ul>
                     </div>
                 </div>
 
-                <ul className='hidden lg:flex items-center gap-10 m-0 p-5'>
-                    <li className='rounded-xl text-[rgb(71,255,47)] font-semibold px-3 py-1  hover:text-blue-500'><a href='#main'>Home</a></li>
-                    <li className='rounded-xl text-[rgb(71,255,47)] font-semibold px-3 py-1  hover:text-blue-500'><a href='#about'>About</a></li>
-                    <li className='rounded-xl text-[rgb(71,255,47)] font-semibold px-3 py-1  hover:text-blue-500'><a href='#events'>Events</a></li>
-                    <li className='rounded-xl text-[rgb(71,255,47)] font-semibold px-3 py-1  hover:text-blue-500'><a href='#projects'>Projects</a></li>
-                    <li className='rounded-xl text-[rgb(71,255,47)] font-semibold px-3 py-1  hover:text-blue-500'><a href='#contact'>Contact</a></li>
-                    <li className='rounded-xl text-black bg-[rgb(71,255,47)] font-semibold px-3 py-1 text-center'><a href='http://google.com'>E-Cycle</a></li>
+                <ul className='hidden lg:flex items-center gap-3 m-0 p-5 mr-10'>
+                    <li className='rounded-xl  font-semibold px-3 py-1  hover:text-[rgb(71,255,47)]'><a href='#main'>Home</a></li>
+                    <li className='rounded-xl  font-semibold px-3 py-1  hover:text-[rgb(71,255,47)]'><a href='#about'>About</a></li>
+                    <li className='rounded-xl  font-semibold px-3 py-1  hover:text-[rgb(71,255,47)]'><a href='#events'>Events</a></li>
+                    <li className='rounded-xl  font-semibold px-3 py-1  hover:text-[rgb(71,255,47)]'><a href='#projects'>Projects</a></li>
+                    <li className='rounded-xl  font-semibold px-3 py-1  hover:text-[rgb(71,255,47)]'><a href='#contact'>Contact</a></li>
+
                 </ul>
             </div>
         </div>
